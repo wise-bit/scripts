cfeee3eddd8eba1ba091470e7c7bc809ed1dfe5a
wise-bit <satrajit2016@gmail.com>
2025-01-04 20:47:47 -0500

    resize pdf script

---
+++ resize_pdf.sh
@@ -0,0 +1,6 @@
+gs -sDEVICE=pdfwrite -dCompatibilityLevel=1.4 -dPDFSETTINGS=/prepress -dNOPAUSE -dQUIET -dBATCH -sOutputFile=compressed_PDF_file.pdf input_PDF_file.pdf
+
+# /prepress - 300 dpi
+# /ebook - 150 dpi
+# /screen - 72 dpi
+
